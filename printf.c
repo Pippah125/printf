029fad2fa1af6885e5dabd7ad8e8490d8e40b0a8
Pippah Gift <Pippah.gift@gmail.com>
2023-04-28 07:53:36 -0700

    alx

--- printf.c
+++ printf.c
@@ -4,10 +4,8 @@ int _printf(const char *format, ...)
 {
 	int x;
 	int y = 0;
-
 	va_list print;
 	va_start(print,format);
-	va_arg(print, int)
 	for (x = 0; format[x] != '\0'; x++)
 	{
 		if (format[x] == '\0')
