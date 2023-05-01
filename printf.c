9b95d0bdb36edb0ea6406e664dfbbb5092c4c60c
Pippah Gift <Pippah.gift@gmail.com>
2023-05-01 06:26:12 -0700

    alx

--- printf.c
+++ printf.c
@@ -17,10 +17,6 @@ int _printf(const char *format, ...)
 		{
 			_putchar(format[x]);
 		}
-		else if (format[x] == '%')
-		{
-			x++;
-		}
 		else if(format[x +1] == 'c')
 		{
 			_putchar(va_arg(print, int));
